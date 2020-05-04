cd4e82700a9d4f9cb97c64d51a2d3063e5cc3462
ksrawr <kenneth.surban62@gmail.com>
2020-05-04 13:18:16 -0700

    adds client socket event to get all active users

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,45 @@ console.log(' i am app js');
 
 let state = {
 	username: '',
-	socket: io(),
+	email: '',
+	socket: io()
 }
 
+const setState = (obj, callback) => {
+	state = { ...state, ...obj};
+	console.log({msg: 'set state', state});
+
+	if(callback) callback();
+}
+
+const setSocketInfo = () => {
+	state.socket.emit('session', {username: state.username, email: state.email}, (error) => {
+
+		if(error) console.warn({error});
+
+	})
+
+	state.socket.on('current users', (data) => {
+		console.log({data});
+	})
+}
+
+const getUserInfo = async () => {
+	try {
+		const id = localStorage.getItem('uid');
+		const response = await fetch(`/api/v1/users/${id}`);
+
+		const data = response.json();
+
+		const { name, status, email } = data.data;
+
+		if(status === 200) {
+			setState({
+				'username': name
+				'email': email
+			}, setSocketInfo)
+		}
+	} catch(error) {
+		console.warn({error});
+	}
+}
